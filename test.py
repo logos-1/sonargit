cc55febed4b9447e2b25b07c375e69d331e80c07
logos-1 <yjlee@logossoft.co.kr>
2025-09-19 09:40:28 +0900

    Jenkins 자동 빌드 테스트

--- test.py
+++ test.py
@@ -1,23 +1,4 @@
-# 너무 긴 함수, 복잡도 높음, 사용하지 않는 변수, 불필요한 주석 포함
+print("Hello World")
 
-def complex_function(a, b, c):
-    # 불필요한 주석
-    result = 0
-    unused_var = 42
-    if a > 0:
-        if b > 0:
-            if c > 0:
-                result = a + b + c
-            else:
-                result = a + b - c
-        else:
-            result = a - b + c
-    else:
-        if b > 0:
-            result = -a + b + c
-        else:
-            result = -a - b + c
-    return result
+# 이건 테스트용 주석입니다
 
-def no_docstring_function(x):
-    return x * 2
