90ae86dbf4b2487f59ba9616530c259bb1d82bee
logos-1 <yjlee@logossoft.co.kr>
2026-01-20 09:56:26 +0900

    Fix S6969: Add memory parameter to Scikit-Learn Pipeline

--- sklearn_test.py
+++ sklearn_test.py
@@ -1,8 +1,12 @@
+import tempfile
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
 
+# 캐시를 위한 임시 디렉토리 생성
+cachedir = tempfile.mkdtemp()
+
 pipeline = Pipeline([
     ('scaler', StandardScaler()),
     ('classifier', LinearDiscriminantAnalysis())
-]) # Noncompliant: the memory parameter is not provided
+], memory=cachedir)
